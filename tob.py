c5276caff0d5c5d173d08eeefbf9f2d701d6503b
João Mauricio Cardoso Gueiros <94720652+jmktt@users.noreply.github.com>
2022-12-19 20:11:34 -0300

    Update tob.py

--- tob.py
+++ tob.py
@@ -1,5 +1,6 @@
 import os
 import random
+import time
 invoption_text = ("\n\033[;1m\x1b[31m There is no such option...\033[0;0m\n")
 
 def main(i=0):
@@ -28,8 +29,9 @@ def main(i=0):
     elif (option == 2):
         print("IN PROGRESS\n")
     elif (option ==3):
-        def gen_key(): #-------------# gen key
+        def gen_key(i=0): #------------# gen key
             os.system("clear")
+            invoption = i
             random.seed()
             key = random.randrange(000000000000000000000000, 999999999999999999999999)
             print("\033[1;97m\n\n              8 8 8 8                     ,ooo.     ")
@@ -42,15 +44,17 @@ def main(i=0):
             print("\n1) Back")
             print("2) ReGen")
             print("\n99) Exit\n")
+            if (invoption == 1):
+                print(invoption_text)
             option = int(input("\nSelect: "))
             if (option == 1):
                 main()
             elif (option == 2):
-                gen_key()
+                gen_key(i=0)
             elif (option == 99):
                 print("\033[1;36mBye...\033[0;0m\n")
             else:
-                print("\033[1;36mExit. Bye...\033[0;0m\n") 
+                gen_key(i=1) 
         gen_key()
     elif (option == 99):
         print("\033[1;36mBye...\033[0;0m\n")
