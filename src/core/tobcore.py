6709e01aaae481227e1de0d3453f5d7e2710cd51
João Mauricio Cardoso Gueiros <94720652+jmktt@users.noreply.github.com>
2022-12-23 18:30:12 -0300

    Create tobcore.py

---
+++ src/core/tobcore.py
@@ -0,0 +1,7 @@
+import os
+def check_os():
+    if os.name == "nt":
+        operating_system = "windows"
+    if os.name == "posix":
+        operating_system = "posix"
+    return(operating_system)
