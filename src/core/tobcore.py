05ae3c97cdb7af27a98728ef190aa35d22875a24
João Mauricio Cardoso Gueiros <94720652+jmktt@users.noreply.github.com>
2022-12-27 16:51:23 -0300

    .

--- src/core/tobcore.py
+++ src/core/tobcore.py
@@ -10,6 +10,13 @@ class bcolors:
     YELLOW = '\033[1;33m'
     RESET = '\033[0;0m'
 
+def write(encrypted_text):
+    w = open("criptout.txt", "a")
+    w.write("\n Cript Text: {0}".format(encrypted_text))
+    w.close()
+
+
+
 ### invalid mansages
 invoption_text = ("\n\033[;1m\x1b[31m There is no such option...\033[0;0m\n")
 invkey_text = ("\n\033[;1m\x1b[31m This key is not valid...\033[0;0m")
