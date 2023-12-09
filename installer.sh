459b3d43ad0255177637dd9ec2897c16705a7d89
João Mauricio Cardoso Gueiros <94720652+jmktt@users.noreply.github.com>
2023-12-09 17:24:50 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -8,7 +8,7 @@ NC='\033[0m'  # no color
 
 # verify root
 if [[ $EUID -ne 0 ]]; then
-    echo -e "${RED}This script needs to be run as root. Run with sudo.${NC}"
+    echo -e "${RED}[*] This script needs to be run as root. Run with sudo.${NC}"
     exit 1
 fi
 
@@ -16,11 +16,11 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # verify python3
-command -v python3 >/dev/null 2>&1 || { echo -e "${RED}Python3 is not installed. Please install Python3.${NC}"; exit 1; }
+command -v python3 >/dev/null 2>&1 || { echo -e "${RED}[!] Python3 is not installed. Please install Python3.${NC}"; exit 1; }
 
 # intall requirements silently
 pip3 install --quiet -r "$DIR/requirements.txt" >/dev/null 2>&1
-echo -e "'requirements.txt'${GREEN} installation done.${NC}"
+echo -e "${GREEN}[*]${NC}'requirements.txt'${GREEN} installation done.${NC}"
 
 # create dir
 mkdir -p /usr/bin/tob-cript
@@ -35,17 +35,17 @@ chmod +x /usr/bin/tob-cript/tob.py
 export PATH=$PATH:/usr/bin/tob-cript/
 
 # add to bashrc for permanent change
-echo "export PATH=\$PATH:/usr/local/bin/tob-cript/" >> "$HOME/.bashrc"
+echo "export PATH=\$PATH:/usr/bin/tob-cript/" >> "$HOME/.bashrc"
 
 # link tob.py to tob if the link doesn't already exist
 if [ -e /usr/bin/tob-cript/tob ]; then
-    echo -e "${YELLOW}Link ${NC}'tob'${YELLOW} already exists."
+    echo -e "${YELLOW}[*] Link ${NC}'tob'${YELLOW} already exists."
 else
     ln -s /usr/bin/tob-cript/tob.py /usr/bin/tob-cript/tob
-    echo -e "${GREEN}Link ${NC}'tob' ${GREEN}created."
+    echo -e "${GREEN}[*] Link ${NC}'tob' ${GREEN}created."
 fi
 
-echo -e "${GREEN}Installation complete. Now you can run${NC} 'tob'."
+echo -e "${GREEN}[!] Installation complete. Now you can run${NC} 'tob'."
 echo -e "${YELLOW}Note: Please restart your terminal for the changes to take effect.${NC}"
 
 exit 0
