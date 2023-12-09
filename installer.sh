924254e3cb9d6a881ee92e90751584b781882cc1
João Mauricio Cardoso Gueiros <94720652+jmktt@users.noreply.github.com>
2023-12-09 15:16:12 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# ANSI COLORS
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m'  # no color
+
 # verify root
 if [[ $EUID -ne 0 ]]; then
-    echo "This script needs to be run as root. Run with sudo."
+    echo -e "${RED}This script needs to be run as root. Run with sudo.${NC}"
     exit 1
 fi
 
@@ -10,26 +16,36 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # verify python3
-command -v python3 >/dev/null 2>&1 || { echo >&2 "Python3 is not installed. Please install Python3."; exit 1; }
+command -v python3 >/dev/null 2>&1 || { echo -e "${RED}Python3 is not installed. Please install Python3.${NC}"; exit 1; }
 
-# intall requirements
-pip3 install -r "$DIR/requirements.txt"
+# intall requirements silently
+pip3 install --quiet -r "$DIR/requirements.txt" >/dev/null 2>&1
+echo -e "'requirements.txt'${GREEN} installation done.${NC}"
 
 # create dir
-mkdir -p /usr/local/bin/tob-cript
+mkdir -p /usr/bin/tob-cript
 
 # copy tob
-cp -ru "$DIR"/* /usr/local/bin/tob-cript/
+cp -ru "$DIR"/* /usr/bin/tob-cript/
 
 # permission
-chmod +x /usr/local/bin/tob-cript/tob.py
+chmod +x /usr/bin/tob-cript/tob.py
 
-# add to bash
+# add to bash for the current session
+export PATH=$PATH:/usr/bin/tob-cript/
+
+# add to bashrc for permanent change
 echo "export PATH=\$PATH:/usr/local/bin/tob-cript/" >> "$HOME/.bashrc"
 
-# link tob.py to tob
-ln -s /usr/local/bin/tob-cript/tob.py /usr/local/bin/tob-cript/tob
+# link tob.py to tob if the link doesn't already exist
+if [ -e /usr/bin/tob-cript/tob ]; then
+    echo -e "${YELLOW}Link ${NC}'tob'${YELLOW} already exists."
+else
+    ln -s /usr/bin/tob-cript/tob.py /usr/bin/tob-cript/tob
+    echo -e "${GREEN}Link ${NC}'tob' ${GREEN}created."
+fi
 
-echo "Installation complete. Now you can run 'tob'."
+echo -e "${GREEN}Installation complete. Now you can run${NC} 'tob'."
+echo -e "${YELLOW}Note: Please restart your terminal for the changes to take effect.${NC}"
 
 exit 0
