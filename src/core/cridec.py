099d974c99757d3a44ab32317d7ab58fea25d77a
João Mauricio Cardoso Gueiros <joaomcg0505@gmail.com>
2024-01-29 14:28:17 -0300

    .

--- src/core/cridec.py
+++ src/core/cridec.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from src.core.tobcore import *
 
 #cript function
--- src/core/tobcore.py
+++ src/core/tobcore.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python3
+
 import os
 import datetime
 import requests
+import zipfile
+import io
 from pystyle import *
 
 def check_os():
@@ -10,17 +14,71 @@ def check_os():
         operating_system = "1"
     return(operating_system)
 
-##def check_update():
+#################################### check up date versions
+repo_owner = "jmktt"
+repo_name = "tob-cript"
+
+def get_latest_release():
+    try:
+        url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases/latest"
+        response = requests.get(url)
+        response.raise_for_status()
+        latest_release = response.json()["tag_name"]
+        return latest_release
+    except Exception as e:
+        print(Col.red+"[!]Error getting latest release:"+bcolors.RESET, e)
+        return None
+
+def get_local_version():
+    try:
+        with open("VERSION", "r") as version_file:
+            local_version = version_file.read().strip()
+            return local_version
+    except Exception as e:
+        print(Col.red+"[!]Error getting local version:"+bcolors.RESET, e)
+        return None
+
+def update_tob(version):
+    try:
+        print("[-]Downloading the latest version...")
+        url = f"https://github.com/{repo_owner}/{repo_name}/archive/{version}.zip"
+        response = requests.get(url)
+        response.raise_for_status()
+        
+        print("[-]Unzipping the ZIP file...")
+        with zipfile.ZipFile(io.BytesIO(response.content)) as zip_file:
+            zip_file.extractall(".")
+        
+        with open("VERSION", "w") as version_file:
+            version_file.write(version)
+        
+        print(Col.green+"[!]TOB updated successfully!"+bcolors.RESET)
+    except Exception as e:
+        print(Col.red+"[!]Error updating TOB:"+bcolors.RESET, e)
+
+def check_update():
+    try:
+        latest_release = get_latest_release()
+        local_version = get_local_version()
+        
+        if local_version == latest_release:
+            print(Col.green+"[-]TOB is up to date."+bcolors.RESET)
+        else:
+            print(Col.red+"[!]There is a newer version available:"+bcolors.RESET, latest_release)
+            update_tob(latest_release)
+    except Exception as e:
+        print(Col.red+"[!]Error checking for update:"+bcolors.RESET, e)
 
 def check_status():
     url = "https://api.github.com"
     try:
         response = requests.head(url, timeout=3)
         response.raise_for_status()
-        print((bcolors.YELLOW+"Internet Status: Online"+bcolors.RESET))
-        #check_update()
+        print(net_online)
+        check_update()
     except requests.RequestException:
-        print((bcolors.YELLOW+"Internet Status: Offline"+bcolors.RESET))
+        print(net_offline)
+##############################################################
 
 operating_system = check_os()
 def disp_clean():
@@ -30,7 +88,7 @@ def disp_clean():
         os.system("cls")
     else:
         print("[!]UNIDENTIFIED OS")
-        
+###############################################################
 class bcolors:
     YELLOW = '\033[1;33m'
     RESET = '\033[0;0m'
@@ -42,7 +100,7 @@ def write(encrypted_text):
     file.close()
 
 
-### banners
+############### banners
 menu_banner = '''
     ooooooooooooo   .oooooo.   oooooooooo.
    8'   888   `8  d8P'  `Y8b  `888'   `Y8b
@@ -99,14 +157,20 @@ def display_key_box(key):
 
 tname = Colorate.Horizontal(Colors.yellow_to_red, "TOB",1)
 cname = Colorate.Horizontal(Colors.red_to_blue, "JMKTT",2)
-version= Colorate.Horizontal(Colors.red_to_blue, "1.2",2)
+version= Colorate.Horizontal(Colors.red_to_blue, "1.4",2)
+###############################################################
 
-### invalid mansages
+### invalid mensages
 invoption_text = (Col.red+"\nThere is no such option...\033[0;0m\n")
 invkey_text = (Col.red+"\n This key is not valid...\033[0;0m")
 invfile_text = (Col.red+"\n File not found...\033[0;0m")
+### status mensages
+net_online = (bcolors.YELLOW+"[+]Internet Status: Online"+bcolors.RESET)
+net_offline = (bcolors.YELLOW+"[-]Internet Status: Offline"+bcolors.RESET)
 
 
+
+######################## loading bar
 
 def loading_bar(iteration, total, prefix='', suffix='', decimals=0, length=100, fill=Colorate.Vertical(Colors.red_to_purple, "_",1)):
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
