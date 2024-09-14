3401386b9f69afcd022d4305f11c88c66e634087
João Mauricio Cardoso Gueiros <joaomcg0505@gmail.com>
2024-09-14 15:51:44 -0300

    .

--- src/core/cridec.py
+++ src/core/cridec.py
@@ -1,36 +1,53 @@
 #!/usr/bin/env python3
 
 from src.core.tobcore import *
+import bip32utils
+from mnemonic import Mnemonic
 
-#cript function
+
+
+
 def encrypt(text, key):
-    result= ""
+    result = ""
+    if key.isdigit():  ## check if the key is made up of digits only
+        key_as_int = int(key)  ## convert key for int number
+        key_as_digits = key_as_int % 10000  # 10000
+    else: #numbers and letters key
+        key_as_digits = ''.join(str(ord(char)).zfill(3) for char in key)  # key characters to numbers and ensuring each number has 3 digits
+        key_as_digits = int(key_as_digits[:24])  # first 24 digits of the converted key
+        key_as_digits = key_as_digits % 10000  # 10000
     l = len(text)
     i = 0
     for char in text:
-        char_code = ord(char) # get ascii value
-        key = key % 10000
-        encrypt_code = (char_code + key) % 0x110000 #range(0x110000) unicode
-        encrypted_char = chr(encrypt_code) # covert back to char
+        char_code = ord(char)  # get ascii of char
+        encrypt_code = (char_code + key_as_digits) % 0x110000  # cript with key in numbers
+        encrypted_char = chr(encrypt_code)  # back to char
         result += encrypted_char
         i += 1
         if i == l:
             loading_bar(i, l, prefix='', suffix='Complete', length=50)
         else:
             loading_bar(i, l, prefix='', suffix='', length=50)
-        #result += hashcript(result)
     return result
+
+
 
 #decrypt function
 def decrypt(text, key):
     result = ""
+    if key.isdigit():  # check if the key is made up of digits only
+        key_as_int = int(key)  # convert key for int number
+        key_as_digits = key_as_int % 10000  # Reduzir o valor para um número gerenciável
+    else: # numbers and letters key
+        key_as_digits = ''.join(str(ord(char)).zfill(3) for char in key)  # key characters to numbers and ensuring each number has 3 digits
+        key_as_digits = int(key_as_digits[:24])  # first 24 digits of the converted key
+        key_as_digits = key_as_digits % 10000  # 10000
     l = len(text)
     i = 0
     for char in text:
-        char_code = ord(char)
-        key = key % 10000
-        decrypted_code = (char_code - key) % 0x110000 #range(0x110000) unicode
-        decrypted_char = chr(decrypted_code) #covert back to char
+        char_code = ord(char)  # get ascii of char
+        decrypted_code = (char_code - key_as_digits) % 0x110000  # decript with key in numbers
+        decrypted_char = chr(decrypted_code)  # back to char
         result += decrypted_char
         i += 1
         if i == l:
@@ -38,5 +55,26 @@ def decrypt(text, key):
         else:
             loading_bar(i, l, prefix='', suffix='', length=50)
     return result
+
+def keys():
+    try:
+        mnemo = Mnemonic("english")
+        ### 32 bytes entropy
+        entropy = os.urandom(32)
+        #### bip 39
+        seed_words = mnemo.to_mnemonic(entropy)
+        #### gen seed
+        seed = mnemo.to_seed(seed_words)
+        master_key = bip32utils.BIP32Key.fromEntropy(seed)
+        xprv = master_key.ExtendedKey()
+        
+        print("\033[1;90mSeed Words (BIP39):\033[0m")
+        print("\033[1;97m%s\033[0m" % seed_words)
+        
+        print("\n\033[1;90mBIP32 Root Key (xprv):\033[0m")
+        print("\033[1;97m%s\033[0m" % xprv)
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 #Author JMCG
--- tob.py
+++ tob.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import secrets
+import string
 from src.core.tobcore import *
 from src.core.cridec import *
 from pystyle import *
+
 
 def main(i=0):
     try:
@@ -19,10 +21,11 @@ def main(i=0):
         print("1) CRYPT")
         print("2) DECRYPT")
         print("3) GEN KEY")
+        print("4) BTC")
         print("\n99) EXIT\n")
         if (invoption == 1):
             print(invoption_text)
-        option = (input("\nSelect: "))
+        option = (input("\n\033[4mtob\033[0m> "))
         if (option == '1'):
         # option 1 - CRIPT
 # option 1 - CRIPT
@@ -41,30 +44,40 @@ def main(i=0):
                 print("\nOptions:")
                 print("1) Input text manually")
                 print("2) Read text from file")
+                print("\n99) Back")
             
-                choice = input("Choose an option: ")
+                choice = (input("\n\033[4mtob\033[0m> "))
             
                 if choice == '1':
                     text = input("\nInput Text: ")
                 elif choice == '2':
                     try:
                         filename = input("Enter the filename: ")
-                        with open(filename, 'r', encoding='utf-8') as file:
-                            text = file.read()
+                        if filename == "tob.py" or filename == "tob":
+                            print(Colorate.Vertical(Colors.green_to_yellow, funny, 2))
+                            print("\n[!] Did you think I wouldn't see this coming?...")
+                            wait = input("Press Enter to back.")
+                            cript()
+                            return 0;
+                        else:   
+                            with open(filename, 'r', encoding='utf-8') as file:
+                                text = file.read()
                     except FileNotFoundError:
                         disp_clean()
                         cript(ik=3)
+                elif choice == '99':
+                    main()
+                    return
                 else:
                     disp_clean()
                     cript(ik=2)
                     return
             
-                while True:  # invalid key looping
+                while True:  # looping de chave inválida
                     try:
-                        key = int(input("Input your KEY: "))
-                        min_valid_key = secrets.randbelow(999999999999999999999999)
-            
-                        if len(str(key)) == len(str(min_valid_key)):
+                        key = input("Input your KEY: ")
+
+                        if len(key) == 24 and all(char in string.ascii_letters + string.digits for char in key):
                             break
                         else:
                             disp_clean()
@@ -75,11 +88,9 @@ def main(i=0):
                         cript(ik=1)
             
                 encrypted_text = encrypt(text, key)
-                write(encrypted_text)  # output in .txt
+                #write(encrypted_text)  # output in .txt
                 print("\nEncrypted text: {0}".format(encrypted_text))
             
-            
-
                 # CRIPT SUB MENU
                 def cript_menu(i=0):
                     invoption=i
@@ -87,11 +98,11 @@ def main(i=0):
                     print("\n99) Exit\n")
                     if (invoption == 1):
                         print(invoption_text)
-                    option = (input("\nSelect: "))
+                    option = (input("\n\033[4mtob\033[0m> "))
                     if (option == '1'):
                         main()
                     elif option == '99'or option == 'exit' or option == 'quit':
-                        print("\033[1;36mBye...\n"+bcolors.RESET)
+                        print(bye_message)
                     else:
                         cript_menu(i=1)
                 cript_menu()
@@ -113,38 +124,48 @@ def main(i=0):
                 print("\nOptions:")
                 print("1) Input text manually")
                 print("2) Read text from file")
+                print("\n99) Back")
                 
-                choice = input("Choose an option: ")
+                choice = (input("\n\033[4mtob\033[0m> "))
                 
                 if choice == '1':
                     text = input("\nInput Text: ")
                 elif choice == '2':
                     try:
                         filename = input("Enter the filename: ")
-                        with open(filename, 'r', encoding='utf-8') as file:
-                            text = file.read()
+                        if filename == "tob.py" or filename == "tob":
+                            print(Colorate.Vertical(Colors.green_to_yellow, funny, 2))
+                            print("\n[!] Did you think I wouldn't see this coming?...")
+                            wait = input("Press Enter to back.")
+                            decript()
+                            return 0;
+                        else:   
+                            with open(filename, 'r', encoding='utf-8') as file:
+                                text = file.read()
                     except FileNotFoundError:
                         disp_clean()
                         decript(ik=3)
+                elif choice == '99':
+                    main()
+                    return
                 else:
                     disp_clean()
                     decript(ik=2)
                     return
                 
-                while True:  # invalid key looping
+                while True:  # looping de chave inválida
                     try:
-                        key = int(input("Input your KEY: "))
-                        min_valid_key = secrets.randbelow(999999999999999999999999)
-                        
-                        if len(str(key)) == len(str(min_valid_key)):
+                        key = input("Input your KEY: ")
+
+                        if len(key) == 24 and all(char in string.ascii_letters + string.digits for char in key):
                             break
                         else:
                             disp_clean()
-                            print(Colorate.Vertical(Colors.purple_to_red, decript_banner, 2))
+                            print(Colorate.Vertical(Colors.purple_to_red, cript_banner, 2))
                             print(invkey_text, "\n")
                     except Exception as e:
                         disp_clean()
-                        decript(ik=1)
+                        cript(ik=1)
                 
                 decrypted_text = decrypt(text, key)
                 print("\nDecrypted text: {0}".format(decrypted_text))
@@ -156,11 +177,11 @@ def main(i=0):
                     print("\n99) Exit\n")
                     if (invoption == 1):
                         print(invoption_text)
-                    option = (input("\nSelect: "))
+                    option = (input("\n\033[4mtob\033[0m> "))
                     if (option == '1'):
                         main()
                     elif option == '99'or option == 'exit' or option == 'quit':
-                        print("\033[1;36mBye...\n"+bcolors.RESET)
+                        print(bye_message)
                     else:
                         decript_menu(i=1)
                 decript_menu()
@@ -170,34 +191,98 @@ def main(i=0):
             def gen_key(i=0):
                 disp_clean()
                 invoption = i
-                key = secrets.randbelow(999999999999999999999999)
+                ##################################
+                if (invoption == 3):
+                    key_length = 24
+                    key = str(secrets.randbelow(10**key_length)).zfill(key_length)### zero fill
+                else:
+                    key_length = 24 ### KEY SIZE
+                    characters = string.ascii_letters + string.digits
+                    key = ''.join(secrets.choice(characters) for _ in range(key_length))  ## increase possibility of keys
                 print(Colorate.Horizontal(Colors.black_to_red,key_banner,1))
                 display_key_box(key)
                 print("\n1) Back")
                 print("2) ReGen")
+                print("3) ReGen (only numbers)")
                 print("\n99) Exit\n")
                 #keygen sub menu
                 if (invoption == 1):
                     print(invoption_text)
-                option = (input("\nSelect: "))
+                option = (input("\n\033[4mtob\033[0m> "))
                 if (option == '1'):
                     main()
                 elif (option == '2'):
                     gen_key(i=0)
+                elif (option == '3'):
+                    gen_key(i=3)
                 elif option == '99'or option == 'exit' or option == 'quit':
-                    print("\033[1;36mBye...\n"+bcolors.RESET)
+                    print("\033[1;36m"+bye_message+bcolors.RESET)
                 else:
                     gen_key(i=1) 
             gen_key()
+################### bip39 
+        elif option == '4':
+            def bip(ik=0):
+                disp_clean()
+                invkey = ik
+                print(Colorate.Horizontal(Colors.black_to_white,bit_logo,2))  
+                
+                if invkey == 1:
+                    print(invoption_text)
+            
+                print("\nOptions:")
+                print("1) Generate BIP39 Keys")
+                print("\n99) Back")
+            
+                choice = (input("\n\033[4mtob\033[0m> "))
+                
+                if (choice == '1'):
+                    def bip_menu(i=0): #gen bip submenu
+                        disp_clean()
+                        print(Colorate.Horizontal(Colors.white_to_blue,bit_logo,2))  
+                        print("")
+                        keys()
+                        invoption=i
+                        print("\nOptions:")
+                        print("\n1) Back")
+                        print("2) ReGen")
+                        print("\n99) Exit")
+
+                        if (invoption == 1):
+                            print(invoption_text)
+                        option = (input("\n\033[4mtob\033[0m> "))
+                        if (option == '1'):
+                            bip()
+                        if (option == "2"):
+                            bip_menu()
+                        elif option == '99'or option == 'exit' or option == 'quit':
+                            print(bye_message)
+                        else:
+                            bip_menu(i=1)
+                    bip_menu()
+                elif choice == '99':
+                    main()
+                else:
+                    bip(ik=1) 
+            bip(ik=0)
+##########################         
         elif option == '99'or option == 'exit' or option == 'quit':
-            print("\033[1;36mBye...\n"+bcolors.RESET)
+            print(bye_message)
+        elif option == "john":
+            print(Colorate.Vertical(Colors.red_to_black, john, 2))
+            credit_text()
+            wait = input("\nPress Enter to back to main menu.")
+            main()
         else:
             main(i=1)
     except KeyboardInterrupt:
         print("\n[!] Control-C detected. Exiting TOB.")
+        print(bye_message)
+        quit
+
     except Exception as e:
         print("\n[!] Something went wrong. Printing the error: {0}".format(e))
-
+        print("[!] Looks like our code decided to pause for a joke!")
 #---------------------# start
 check_status()
 main() 
